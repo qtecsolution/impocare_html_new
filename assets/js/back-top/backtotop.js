4d5c2b04ca59866b6afb33eb4f438cf769d79f89
it27427 <istiak.tushar.qtec@gmail.com>
2023-06-01 11:49:52 +0600

    footer-markup donw

--- assets/js/back-top/backtotop.js
+++ assets/js/back-top/backtotop.js
@@ -1,6 +1,6 @@
 up({
   // bottom position
-  bottom: '1.5rem',
+  bottom: '5rem',
   // right position
   right: '1.5rem',
   // width
@@ -8,7 +8,7 @@ up({
   // height
   height: '2.5rem',
   // background color
-  bg: 'hsl(231, 71%, 15%)',
+  bg: 'hsl(198, 76%, 47%)',
   // custom icon
   // src: '../../assets/cursor/chevron-up-solid.svg',
   // distance from the top to show the back to top button
