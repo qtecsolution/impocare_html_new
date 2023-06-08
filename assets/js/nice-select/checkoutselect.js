9e3db873a78be064e812e0de6a1ada6fa30245b1
it27427 <istiak.tushar.qtec@gmail.com>
2023-06-08 11:24:28 +0600

    order-details done

---
+++ assets/js/nice-select/checkoutselect.js
@@ -0,0 +1,9 @@
+NiceSelect.bind(document.getElementById("districts"), {
+  searchable: false,
+  placeholder: 'Dhaka'
+});
+
+NiceSelect.bind(document.getElementById("deliverymethod"), {
+  searchable: false,
+  placeholder: 'Office Collection'
+});
---
+++ assets/js/nice-select/orderselect.js
@@ -0,0 +1,9 @@
+NiceSelect.bind(document.getElementById("divisions"), {
+  searchable: false,
+  placeholder: 'Select your division'
+});
+
+NiceSelect.bind(document.getElementById("districts"), {
+  searchable: false,
+  placeholder: 'Select your district'
+});
